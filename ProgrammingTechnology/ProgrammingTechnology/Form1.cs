f0553272cc5fda6e8d5d62458a85a55cd82a84dc
manaleks <man_aleks@mail.ru>
2017-04-08 11:01:24 +0300

    Create Form1.cs

---
+++ ProgrammingTechnology/ProgrammingTechnology/Form1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgrammingTechnology
+{
+    //Класс "Controller"
+    //Представляет собой "мост" между пользовательским интерфейсом и бизнес-логикой программы
+    // 
+    //Ответственный: Мышко Е. 
+
+    public partial class Controller : Form
+    {
+        public Controller()
+        {
+            InitializeComponent();
+        }
+
+        private void Converter_TEST_Click(object sender, EventArgs e)
+        {
+            //Конвертер тестируем ТУТ !!!!!!!!!!!!!!!!!!!!!
+        }
+
+        private void Decorator_TEST_Click(object sender, EventArgs e)
+        {
+            //Декоратор тестируем ТУТ !!!!!!!!!!!!!!!!!!!!!
+        }
+
+        private void Methods_TEST_Click(object sender, EventArgs e)
+        {
+            //Методы тестируем ТУТ !!!!!!!!!!!!!!!!!!!!!
+        }
+
+        private void View_TEST_Click(object sender, EventArgs e)
+        {
+            //Вьюшку тестируем ТУТ !!!!!!!!!!!!!!!!!!!!!
+        }
+    }
+}
