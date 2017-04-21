95a5f30296606b5d5eadc2160ecb60a29b939353
petrgeorgievsky <petrgeorgievsky@gmail.com>
2017-04-21 16:03:12 +0300

    Decorator small update, methods integration

--- ProgrammingTechnology/ProgrammingTechnology/Decorator.cs
+++ ProgrammingTechnology/ProgrammingTechnology/Decorator.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ProgrammingTechnology
 {
@@ -12,10 +10,12 @@ namespace ProgrammingTechnology
     //Ответственный: Георгиевский П. 
 
     // Делегат используемый для методов сортировки, на вход идет неотсортированный массив.
-    public delegate int[] STD_SortingMethod(int[] array);
+    using MethodInfo_Pair = KeyValuePair<string, Func<int[], int[]>>;
 
     static class Decorator
     {
+        private static MethodInfo ExceptionOutOfRangeMethodInfo = new MethodInfo("Метод не найден", -1, -1);
+        private static MethodInfo ExceptionErrorMethodInfo = new MethodInfo("Ошибка при вызове метода", -1, -1);
         /// <summary>
         /// Запускает методы сортировки с идентификаторами из methodIDs для массива array.
         /// </summary>
@@ -25,31 +25,64 @@ namespace ProgrammingTechnology
         public static List<MethodInfo> RunMethods(List<int> methodIDs, int[] array)
         {
             List<MethodInfo> decRes = new List<MethodInfo>();
-            List<KeyValuePair<STD_SortingMethod, string>> methodsDelegates = new List<KeyValuePair<STD_SortingMethod, string>>(); // TODO: Нужен метод возращающий список пар "метод+имя" на 
-                                                                                                                                  // вход которого идет список ID методов.
 
             // Бежим по всем полученым делегатам и вызываем данные методы.
-            foreach (var method in methodsDelegates)
-                decRes.Add(RunMethod(method, array));
+            for (int i = 0; i < methodIDs.Count; i++) {
+                // РУСЯ: плс сделай понормальному сам видишь как это некрасиво
+                try
+                {
+                    MethodInfo_Pair methodInputInfo = new MethodInfo_Pair(Methods.DicIdName[methodIDs[i]], Methods.DicMetSor[Methods.DicIdName[methodIDs[i]]]);
+                    decRes.Add(RunMethod(methodInputInfo, array));
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    decRes.Add(ExceptionOutOfRangeMethodInfo);
+                }
+                catch(Exception e)
+                {
+                    decRes.Add(ExceptionErrorMethodInfo);
+                }
+            }
 
             return decRes;
         }
 
-        // Вызывает метод из пары "метод+имя" methodInputInfo для массива array. TODO: Решить нужно ли возвращать сортированный массив
-        private static MethodInfo RunMethod(KeyValuePair<STD_SortingMethod, string> methodInputInfo, int[] array)
+        // Вызывает метод из пары "метод+имя" methodInputInfo для массива array.
+        private static MethodInfo RunMethod(MethodInfo_Pair methodInputInfo, int[] array)
         {
             MethodInfo outStruct; int[] sortedArray; int time;
 
             time = DateTime.Now.Second;
-            sortedArray = methodInputInfo.Key(array);
+            sortedArray = methodInputInfo.Value(array);
             time -= DateTime.Now.Second;
 
-            outStruct.name      = methodInputInfo.Value;
-            outStruct.time      = time;                 // Время в секундах. TODO: Решить нужно ли хранить время в более точном формате/в виде структуры DateTime.
+            outStruct.name      = methodInputInfo.Key;
+            outStruct.time      = time;                 // Время в секундах.
             outStruct.capacity  = sortedArray.Length;   // Спорный вопрос: на кой черт метод возвращает размер обработанного массива,
                                                         // если мы вызываем все методы для одного массива за раз и этот размер не меняется со временем.
 
             return outStruct;
+        }
+
+        public static Dictionary<string,bool> Test()
+        {
+            Dictionary<string, bool> test_result=new Dictionary<string, bool>();
+            List<int> test_methodIDlist = new List<int>
+            {
+                -1
+            };
+            int[] test_array = new int[] { 0, 1, 3, 2};
+
+            List<MethodInfo> test_resList = RunMethods(test_methodIDlist, test_array);
+            test_result.Add("Проверка на ввод несуществующего метода", test_resList[0].Equals(ExceptionOutOfRangeMethodInfo));
+
+            test_methodIDlist = new List<int>
+            {
+                1,2
+            };
+            test_resList = RunMethods(test_methodIDlist, test_array);
+            test_result.Add("Проверка на неизменность размера массива", test_resList[0].capacity== test_resList[1].capacity);
+            return test_result;
         }
     }
 }
