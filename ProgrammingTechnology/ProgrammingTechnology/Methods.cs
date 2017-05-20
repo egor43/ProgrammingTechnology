4efb499d762e064928ce2fc32a4a467088941f3d
helmetica <helmetica@yandex.ru>
2017-05-20 13:02:13 +0300

    Добавлены Test_MergeSort(), Test_InsertionSort()

--- ProgrammingTechnology/ProgrammingTechnology/Methods.cs
+++ ProgrammingTechnology/ProgrammingTechnology/Methods.cs
@@ -13,9 +13,81 @@ namespace ProgrammingTechnology
 
     static class Methods
     {
-        private static int[] MergeSort(int[] mas) //сортировка слиянием
+        private Dictionary<string,bool> Test_InsertionSort()
         {
-            if (mas.Length == 1) return mas;
+            string name="InsertionSort";
+            int[] t1 = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] t2 = new int[] { };
+            int[] t3 = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            int[] t4 = new int[] { 9, 64, 7, -6, 5, 64, 3, 2, -1, 0 };
+            int[] t5 = new int[] { 5};
+            int[] t6 = new int[] { -5, -10, -8, -7,  -3};
+          
+            Dictionary<string,bool> test = new Dictionary<string,bool>();
+            
+            #if Debug
+            test.Add(name,Check_Array(InsertionSort(t1)));
+             test.Add(name,Check_Array(InsertionSort(t2)));
+             test.Add(name,Check_Array(InsertionSort(t3)));
+             test.Add(name,Check_Array(InsertionSort(t4)));
+             test.Add(name,Check_Array(InsertionSort(t5)));
+             test.Add(name,Check_Array(InsertionSort(t6)));
+            #else
+           test.Add(name,Check_Array(InsertionSort(t1)));
+             test.Add(name,Check_Array(InsertionSort(t2)));
+             test.Add(name,Check_Array(InsertionSort(t3)));
+            #endif
+            return test;
+        }
+        
+        private Dictionary<string,bool> Test_MergeSort()
+        {
+            string name="MergeSort";
+            int[] t1 = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] t2 = new int[] { };
+            int[] t3 = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            int[] t4 = new int[] { 9, 64, 7, -6, 5, 64, 3, 2, -1, 0 };
+            int[] t5 = new int[] { 5};
+            int[] t6 = new int[] { -5, -10, -8, -7,  -3};
+          
+            Dictionary<string,bool> test = new Dictionary<string,bool>();
+            
+            #if Debug
+            test.Add(name,Check_Array(MergeSort(t1)));
+             test.Add(name,Check_Array(MergeSort(t2)));
+             test.Add(name,Check_Array(MergeSort(t3)));
+             test.Add(name,Check_Array(MergeSort(t4)));
+             test.Add(name,Check_Array(MergeSort(t5)));
+             test.Add(name,Check_Array(MergeSort(t6)));
+            #else
+           test.Add(name,Check_Array(MergeSort(t1)));
+             test.Add(name,Check_Array(MergeSort(t2)));
+             test.Add(name,Check_Array(MergeSort(t3)));
+            #endif
+            return test;
+        }
+
+        //Омеличева Наталья (Сортировка вставками)
+        private static int[] InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int value = array[i]; // текущее значение
+                int j = i;
+                while (j > 0 && array[j - 1] > value) // идем по массиву влево и переставляем текущее значение до тех пор, пока оно не станет больше значения слева
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+                array[j] = value;
+            }
+            return array;
+        }
+
+        //Омеличева Наталья (Сортировка слиянием)
+        private static int[] MergeSort(int[] mas)
+        {
+            if (mas.Length <= 1) return mas;
             int middle = mas.Length / 2;
 
             // массивы для разбиения входного массива на две части
@@ -54,7 +126,7 @@ namespace ProgrammingTechnology
 
             return sorted;
         }
-
+        
         private static int[] GnomeSort(int[] Input)
         {
             int index = 0;
