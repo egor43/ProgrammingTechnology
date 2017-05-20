d5f4e4e3e61bac08e0e3ff411713ed92303101bd
IosephKnecht <Andreich.mezya67@ya.ru>
2017-05-20 12:39:31 +0300

    Update Methods.cs

--- ProgrammingTechnology/ProgrammingTechnology/Methods.cs
+++ ProgrammingTechnology/ProgrammingTechnology/Methods.cs
@@ -373,5 +373,14 @@ namespace ProgrammingTechnology
             return tmp;
         }
         
+        private bool Check_Array(int [] input) 
+        {
+            for(int i=0;i<input.Length-1;i++)
+            {
+                if(input[i]>input[i+1]) return false;
+            }
+            return false;
+        }
+        
     }
 }
